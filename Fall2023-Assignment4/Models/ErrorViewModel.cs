742e55365b8cf360856555ea8238785f46d12908
ttdo19 <thuytrang19072001@gmail.com>
2023-10-25 17:31:42 -0500

    Add initial code from VS

---
+++ Fall2023-Assignment4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Fall2023_Assignment4.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
